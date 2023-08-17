92c31e767c3e25ecab645a9efa18c70bed0805de
David Hill <david@davidhill.ie>
2023-08-17 22:35:01 +0100

    Add alert

--- components/MyButton.jsx
+++ components/MyButton.jsx
@@ -1,6 +1,7 @@
 function MyButton(props) {
   const clickHandler = (evt) => {
     console.log(evt);
+    alert("Client side behaviour");
   };
 
   return <button onClick={clickHandler}>{props.children || "Default"}</button>;
