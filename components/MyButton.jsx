86abbba192cef3818644e5a27cc91c0dae0ff6bd
David Hill <david@davidhill.ie>
2023-08-17 22:47:15 +0100

    Style button

--- components/MyButton.jsx
+++ components/MyButton.jsx
@@ -1,10 +1,16 @@
+import styles from "./MyButton.module.css";
+
 function MyButton(props) {
   const clickHandler = (evt) => {
     console.log(evt);
     alert("Client side behaviour");
   };
 
-  return <button onClick={clickHandler}>{props.children || "Default"}</button>;
+  return (
+    <button className={styles.myButton} onClick={clickHandler}>
+      {props.children || "Default"}
+    </button>
+  );
 }
 
 export default MyButton;
